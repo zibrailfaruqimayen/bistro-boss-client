93bf04f82391134247cd936728a0013d7cea3dca
zibrailfaruqimayen <zibrail.faruqi@gmail.com>
2023-05-23 04:00:40 +0600

    banner added

--- src/Pages/Shared/NavBar/NavBar.jsx
+++ src/Pages/Shared/NavBar/NavBar.jsx
@@ -33,7 +33,7 @@ const NavBar = () => {
   );
   return (
     <>
-      <div className="navbar bg-base-100">
+      <div className="navbar max-w-screen-xl fixed z-10 bg-opacity-30 bg-black text-white">
         <div className="navbar-start">
           <div className="dropdown">
             <label tabIndex={0} className="btn btn-ghost lg:hidden">
