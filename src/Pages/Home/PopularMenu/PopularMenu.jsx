077190638b5afea65850d3ed975e140e39eeffae
zibrailfaruqimayen <zibrail.faruqi@gmail.com>
2023-05-23 22:48:04 +0600

    rating

--- src/Pages/Home/PopularMenu/PopularMenu.jsx
+++ src/Pages/Home/PopularMenu/PopularMenu.jsx
@@ -23,6 +23,11 @@ const PopularMenu = () => {
           <MenuItem key={item._id} item={item}></MenuItem>
         ))}
       </div>
+      <div className="flex justify-center items-center">
+        <button className="btn btn-outline border-b-4  border-0 mt-4  hover:text-black hover:text-white">
+          View Full Menu
+        </button>
+      </div>
     </section>
   );
 };
