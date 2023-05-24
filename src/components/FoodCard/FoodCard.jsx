4a244e88813c1c4f1ac032885102cd40f9bdfe6b
zibrailfaruqimayen <zibrail.faruqi@gmail.com>
2023-05-24 21:44:57 +0600

    end

--- src/components/FoodCard/FoodCard.jsx
+++ src/components/FoodCard/FoodCard.jsx
@@ -8,7 +8,7 @@ const FoodCard = ({ item }) => {
       <p className="absolute right-0 mr-4 mt-4 px-4 bg-slate-900 rounded-xl text-white">
         ${price}
       </p>
-      <div className="card-body">
+      <div className="card-body flex flex-col items-center">
         <h2 className="card-title">{name}</h2>
         <p className="text-center">{recipe}</p>
         <div className="card-actions justify-end">
