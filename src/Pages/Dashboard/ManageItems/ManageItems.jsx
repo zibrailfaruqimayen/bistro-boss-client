e302b43b23cf41bc4133b1a6e81741560a1e004e
zibrailfaruqimayen <zibrail.faruqi@gmail.com>
2023-06-01 16:50:42 +0600

    end

--- src/Pages/Dashboard/ManageItems/ManageItems.jsx
+++ src/Pages/Dashboard/ManageItems/ManageItems.jsx
@@ -1,9 +1,35 @@
 import { FaRegEdit, FaTrashAlt } from "react-icons/fa";
 import useMenu from "../../../Hooks/useMenu";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import Swal from "sweetalert2";
+import useAxiosSecure from "../../../Hooks/useAxiosSecure";
 
 const ManageItems = () => {
-  const [menu] = useMenu();
+  const [menu, , refetch] = useMenu();
+  const [axiosSecure] = useAxiosSecure();
+
+  const handleDelete = (item) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        axiosSecure.delete(`/menu/${item._id}`).then((res) => {
+          console.log("delete res", res.data);
+          if (res.data.deletedCount > 0) {
+            refetch();
+            Swal.fire("Deleted!", "Your file has been deleted.", "success");
+          }
+        });
+      }
+    });
+  };
+
   return (
     <div className="w-full px-10">
       <SectionTitle
@@ -26,9 +52,9 @@ const ManageItems = () => {
           <tbody>
             {menu.map((item, index) => (
               <tr key={item._id}>
-                <th>
+                <td>
                   <label>{index + 1}</label>
-                </th>
+                </td>
                 <td>
                   <div className="flex items-center space-x-3">
                     <div className="avatar">
@@ -43,14 +69,17 @@ const ManageItems = () => {
                 </td>
                 <td>{item.name}</td>
 
-                <td>${item.price}</td>
+                <td className="text-right">${item.price}</td>
                 <td>
                   <button className="btn btn-ghost text-white text-lg  bg-[#D1A054] hover:bg-[#f8ac3b] ">
                     <FaRegEdit></FaRegEdit>
                   </button>
                 </td>
                 <td>
-                  <button className="btn btn-ghost text-white bg-red-500 hover:bg-red-900">
+                  <button
+                    onClick={() => handleDelete(item)}
+                    className="btn btn-ghost text-white bg-red-500 hover:bg-red-900"
+                  >
                     <FaTrashAlt></FaTrashAlt>
                   </button>
                 </td>
